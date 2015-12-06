7038eacac7ea1b5131ad19b14fca80bef6c4b18b
Aaron <AaronFChan@gmail.com>
2015-12-06 05:53:47 -0800

    setup activity layout

--- BillSplit/app/src/main/java/pekkles/billsplit/activity/SetupActivity.java
+++ BillSplit/app/src/main/java/pekkles/billsplit/activity/SetupActivity.java
@@ -1,7 +1,9 @@
-package pekkles.billsplit;
+package pekkles.billsplit.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
+
+import pekkles.billsplit.R;
 
 public class SetupActivity extends Activity {
 
@@ -9,6 +11,7 @@ public class SetupActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup);
+
         
     }
 }
