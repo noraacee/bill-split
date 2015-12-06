5e46e3b93c66de4e677e08980ca1b4ebc14ef317
Aaron Chan <AaronFChan@gmail.com>
2015-12-06 04:41:01 -0800

    added layout

--- BillSplit/app/src/main/java/pekkles/billsplit/SetupActivity.java
+++ BillSplit/app/src/main/java/pekkles/billsplit/SetupActivity.java
@@ -1,7 +1,14 @@
 package pekkles.billsplit;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class SetupActivity extends Activity {
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+        
+    }
 }
